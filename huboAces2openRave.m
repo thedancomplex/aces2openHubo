13c419e4d8573c44a14e6398ab8224e3a65578c4
Your Name <dan@danlofaro.com>
2012-03-17 19:40:40 -0400

    all working with reading aces file

--- huboAces2openRave.m
+++ huboAces2openRave.m
@@ -1,21 +1,28 @@
 close all
 clear all
 
+%% sampling rate
+T = 0.01;
 %%i setup initial hubo and world in openRAVE
 huboOpenRAVEsetup
 
-%% sampling rate
-T = 0.01;
 
 %% time start   
-t = 0
+t = 0;
 
 
 %% enable the robot
 orBodyEnable(hubo,1)
 
+%% Load aces file
+[jc, dd] = readAces('Underhand_2012-03-17_1645.txt');
+%[jc, dd] = readAces('jTest.aces');
+sAces = size(dd);
+d = dd(:,1:(sAces(2)-1));
+
+
 %% the joints used
-di 	= 	[ RSP, RSR, RSY, REB  ]';
+di 	= 	jc(1:(length(jc)-1));
 ddi	=	di + 1;	
 
 %% get joint lim
@@ -27,10 +34,15 @@ co = [];
 
 %% the while loop
 theGo = 1;
-while(theGo == 1)
 
+%while(theGo == 1)
+
+for ( i = 1:sAces(1))		% go over whole trajectory
 	%% set dof values
-	orRobotSetDOFValues(hubo,d, di);
+	deg = d(i,:);
+	deg = deg.*orDir(ddi);
+
+	orRobotSetDOFValues(hubo,deg, di);
 	
 	%% step simulation
 	orEnvStepSimulation(T,1);
